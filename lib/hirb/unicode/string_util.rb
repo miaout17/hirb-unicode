416d244f2276d30c391221cf2156c20428c11dc2
BigCat <miaout17@gmail.com>
2011-02-17 23:15:03 +0800

    move file

---
+++ lib/hirb/unicode/string_util.rb
@@ -0,0 +1,31 @@
+require 'hirb'
+require 'unicode/display_width'
+
+module Hirb
+  module Unicode
+    module StringUtil
+      def size(string)
+        string.display_width
+      end
+
+      def slice(string, offset, width)
+        chars = string.chars.to_a[offset..-1].to_a
+
+        current_length = 0
+        split_index = 0
+        chars.each_with_index do |c, i|
+          char_width = self.size(c)
+          break if current_length + char_width > width
+          split_index = i+1
+          current_length += char_width
+        end
+
+        split_index ||= chars.count
+        head = chars[0, split_index].join
+        head
+      end
+    end
+  end
+end
+
+Hirb::String.extend(Hirb::Unicode::StringUtil)
